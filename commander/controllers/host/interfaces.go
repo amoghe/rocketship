9bc6f27318b374413a374fdc18e1424585803b21
Akshay Moghe <akshay@defend7.com>
2015-07-30 23:38:19 -0700

    [commander] Move interface related resources to interface.go file
    Each data model file defines its own resource.

--- commander/controllers/host/interfaces.go
+++ commander/controllers/host/interfaces.go
@@ -368,3 +368,71 @@ func (i *InterfaceConfig) validateIPs() error {
 
 	return nil
 }
+
+//
+// Resources
+//
+
+type DHCPProfileResource struct {
+	DNSMode            string // One of Mode[None|Append|Prepend|Supercede]
+	OverrideHostname   bool   // Whether to supercede the name returned by the dhcp server
+	OverrideDomainName bool   // Whether to supercede the name returned by the dhcp server
+
+	RequireOptions []string // OptionsSeparator separated string
+	RequestOptions []string // OptionsSeparator separated string
+}
+
+func (r *DHCPProfileResource) FromDHCPProfile(d DHCPProfile) error {
+	var (
+		deserializedRequestOpts = []string{}
+		deserializedRequireOpts = []string{}
+	)
+
+	if len(d.RequestOptions) > 0 {
+		err := json.Unmarshal([]byte(d.RequestOptions), &deserializedRequestOpts)
+		if err != nil {
+			return err
+		}
+	}
+	if len(d.RequireOptions) > 0 {
+		err := json.Unmarshal([]byte(d.RequireOptions), &deserializedRequireOpts)
+		if err != nil {
+			return err
+		}
+	}
+
+	r.DNSMode = d.DNSMode
+	r.OverrideHostname = d.OverrideHostname
+	r.OverrideDomainName = d.OverrideDomainName
+	r.RequestOptions = deserializedRequestOpts
+	r.RequireOptions = deserializedRequireOpts
+
+	return nil
+}
+
+func (r *DHCPProfileResource) ToDHCPProfile() (DHCPProfile, error) {
+
+	serializedRequestOpts, err := json.Marshal(r.RequestOptions)
+	if err != nil {
+		return DHCPProfile{}, err
+	}
+	serializedRequireOpts, err := json.Marshal(r.RequireOptions)
+	if err != nil {
+		return DHCPProfile{}, err
+	}
+	if r.DNSMode != ModeAppend && r.DNSMode != ModePrepend && r.DNSMode != ModeOverride {
+		return DHCPProfile{}, fmt.Errorf("Invalid DNSMode")
+	}
+
+	return DHCPProfile{
+		DNSMode:            r.DNSMode,
+		OverrideHostname:   r.OverrideHostname,
+		OverrideDomainName: r.OverrideDomainName,
+		RequireOptions:     string(serializedRequireOpts),
+		RequestOptions:     string(serializedRequestOpts),
+
+		//AppendOptions:    "{}",
+		//PrependOptions:   "{}",
+		//SupercedeOptions: "{}",
+	}, nil
+}
--- commander/controllers/host/interfaces_test.go
+++ commander/controllers/host/interfaces_test.go
@@ -28,7 +28,7 @@ func (ts *InterfacesTestSuite) TearDownTest(c *C) {
 }
 
 //
-// Tests
+// DB Tests
 //
 
 func (ts *InterfacesTestSuite) TestSaveInterfaceConfigShouldFailWithInvalidIPs(c *C) {
@@ -147,4 +147,97 @@ func (ts *InterfacesTestSuite) TestDhclientConfFileGeneration(c *C) {
 	c.Assert(strings.Contains(string(filecontents), "interface eth0 {"), Equals, true)
 	// No section for static interfaces
 	c.Assert(strings.Contains(string(filecontents), "interface test0 {"), Equals, false)
+}
+
+//
+// Resource tests
+//
+
+func (r *InterfacesTestSuite) TestDHCPProfileToResourceConversion(c *C) {
+
+	goodProfiles := []DHCPProfile{
+		DHCPProfile{
+			TimingOptions:    "{}", // Dont care
+			SendOptions:      "{}", // Dont care
+			RequireOptions:   "[]",
+			RequestOptions:   "[]",
+			DNSMode:          ModeAppend,
+			OverrideHostname: true,
+		},
+		DHCPProfile{
+			TimingOptions:    DefaultTimingOptionsJSON,
+			SendOptions:      DefaultSendOptionsJSON,
+			RequireOptions:   DefaultRequestOptionsJSON,
+			DNSMode:          ModeAppend,
+			OverrideHostname: false,
+		},
+	}
+
+	badProfiles := []DHCPProfile{
+		DHCPProfile{
+			RequireOptions:   "[]",
+			RequestOptions:   "asdf", // bad json
+			DNSMode:          ModeAppend,
+			OverrideHostname: true,
+		},
+		DHCPProfile{
+			RequireOptions:   "qwer", // bad json
+			RequestOptions:   "[]",
+			DNSMode:          ModeAppend,
+			OverrideHostname: true,
+		},
+	}
+
+	for _, d := range goodProfiles {
+		resource := DHCPProfileResource{}
+		c.Assert(resource.FromDHCPProfile(d), IsNil)
+		c.Assert(resource.DNSMode, Equals, d.DNSMode)
+		c.Assert(resource.OverrideHostname, Equals, d.OverrideHostname)
+		c.Assert(resource.OverrideDomainName, Equals, d.OverrideDomainName)
+	}
+
+	for _, d := range badProfiles {
+		resource := DHCPProfileResource{}
+		c.Assert(resource.FromDHCPProfile(d), Not(IsNil))
+	}
+}
+
+func (r *InterfacesTestSuite) TestDHCPResourceToProfileConversion(c *C) {
+
+	goodResoures := []DHCPProfileResource{
+		{
+			DNSMode:          ModeAppend,
+			RequireOptions:   []string{"subnet-mask"},
+			RequestOptions:   []string{"subnet-mask", "routers", "domain-name"},
+			OverrideHostname: true,
+		},
+		{
+			DNSMode:          ModePrepend,
+			RequireOptions:   []string{},
+			RequestOptions:   []string{},
+			OverrideHostname: true,
+		},
+	}
+
+	badResoures := []DHCPProfileResource{
+		{
+			DNSMode:          "", // missing mode
+			RequireOptions:   []string{"subnet-mask"},
+			RequestOptions:   []string{"subnet-mask", "routers", "domain-name"},
+			OverrideHostname: true,
+		},
+	}
+
+	for _, resource := range goodResoures {
+		d, err := resource.ToDHCPProfile()
+		c.Assert(err, IsNil)
+		c.Assert(resource.DNSMode, Equals, d.DNSMode)
+		c.Assert(resource.OverrideHostname, Equals, d.OverrideHostname)
+		c.Assert(resource.OverrideDomainName, Equals, d.OverrideDomainName)
+	}
+
+	for _, resource := range badResoures {
+		err, _ := resource.ToDHCPProfile()
+		c.Assert(err, Not(IsNil))
+	}
 }
