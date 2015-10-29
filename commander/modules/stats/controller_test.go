6764d0e48955580b904f10f4996a837279ce0bbb
Akshay Moghe <akshay@defend7.com>
2015-10-29 01:12:35 -0700

    [commander] Add a module to configure stats subsystem on the box.
    Stats are collected using prometheus, which needs to be configured to scrape
    from node_exporter (which is always running on the box).
    
    Also, make node_exporter listen only on localhost.

---
+++ commander/modules/stats/controller_test.go
@@ -0,0 +1,51 @@
+package stats
+
+import (
+	"testing"
+
+	"rocketship/regulog"
+
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/mattn/go-sqlite3"
+	. "gopkg.in/check.v1"
+	"gopkg.in/yaml.v2"
+)
+
+type StatsConfigTestSuite struct {
+	db         *gorm.DB
+	controller *Controller
+}
+
+// Register the test suite with gocheck.
+func init() {
+	Suite(&StatsConfigTestSuite{})
+}
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func (ts *StatsConfigTestSuite) SetUpTest(c *C) {
+	db, err := gorm.Open("sqlite3", "file::memory:?cache=shared")
+	c.Assert(err, IsNil)
+	ts.db = &db
+
+	ts.controller = NewController(ts.db, regulog.NewNull(""))
+	ts.controller.MigrateDB()
+	ts.controller.SeedDB()
+}
+
+func (ts *StatsConfigTestSuite) TearDownTest(c *C) {
+	ts.db.Close()
+}
+
+//
+// Tests
+//
+
+func (ts *StatsConfigTestSuite) TestPrometheusConfigHasValidYAML(c *C) {
+	conf, _ := ts.controller.prometheusFileContents()
+	c.Assert(yaml.Unmarshal(conf, &map[interface{}]interface{}{}), IsNil)
+}
